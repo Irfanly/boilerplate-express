6b9498003a1b1455fd9a26d736ea54867865bb7d
Irfan Zainal <muhdirfan.zainal02@gmail.com>
2025-03-04 10:09:25 +0000

    Add function

--- myApp.js
+++ myApp.js
@@ -1,9 +1,26 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
 
+// app.get("/", GET);
+app.get("/", SERVE_HTML);
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", JSON);
 
+// function GET (req, res){
+//     res.send("Hello Express")
+// }
 
+function SERVE_HTML (req, res){
+    res.sendfile(__dirname + "/views/index.html")
+}
+
+function JSON(req,res){
+    res.json({
+        message : "Hello json"
+    })
+}
 
 
 
